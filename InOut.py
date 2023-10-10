e96fe177c6a6d07f2d93d87054851d6ebbc40685
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-10-09 23:27:21 -0600

    Entrada y Salida python

---
+++ InOut.py
@@ -0,0 +1,20 @@
+# Codigos Basicos de entrad y salida de datos en Python
+
+# Salida de datos
+print("Hello World")
+print("Bienvenidos al repositorio")
+print(9," Octubre ",2023," Se lanzo el repositorio")
+
+# Puedes notar que no re necesitan de ";" al final de cada linea de codigo ya que
+# La sintaxis de python no lo requiere
+
+# Entrada de datos
+
+Nombre=input("Ingresa tu nombre:")
+Edad=int(input("Ingresa tu edad:"))
+Escolaridad=input("Ingresa tu escolaridad:")
+
+# Mostrar Datos obtenidos
+print("Nombre: ",Nombre)
+print("Edad: ",Edad)
+print("Escolaridad: ",Escolaridad)
