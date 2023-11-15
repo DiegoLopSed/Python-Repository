54a21db2ffa19faaae1904fad951278929c81b54
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-14 22:13:24 -0600

    Estructura y Sintaxis de un Arreglo

---
+++ Arreglos_Matrices/Arreglos.py
@@ -0,0 +1,39 @@
+Arreglo1=[] #? Creamos una variable de tipo lista o arreglo usando '[]'
+
+Arreglo1=[10,20,30,40,50] #? Guardamos valores dentro del arreglo
+
+print(Arreglo1) #? Imprime todos los elementos del arreglo
+
+print(Arreglo1[0]) #* Imprime el primer elemento del arreglo
+
+print(Arreglo1[-1]) #* Imprime el ultimo elemento del arreglo
+
+
+
+Arreglo1=["1","2","3","4","5"] #! Guarda elementos de tipo cadena
+
+print(Arreglo1) #? Imprime todos los elementos del arreglo
+
+print(Arreglo1[0]) #* Imprime el primer elemento del arreglo
+
+print(Arreglo1[-1]) #* Imprime el ultimo elemento del arreglo
+
+Arreglo2 = [] #?Creamos un arreglo vacio
+
+print("Arreglo Principal")
+print(Arreglo2)
+
+Arreglo2.append(60) #! Agrega un elemento nuevo en el arreglo
+Arreglo2.append(70) #! Agrega un elemento nuevo en el arreglo
+Arreglo2.append(80) #! Agrega un elemento nuevo en el arreglo
+Arreglo2.append(90) #! Agrega un elemento nuevo en el arreglo
+
+print("Arreglo Principal")
+print(Arreglo2)
+
+
+Arreglo2[0]=350 #? Modificamos el valor de la poscición inicial 
+
+print("Arreglo Modificado")
+print(Arreglo2)
+
---
+++ Arreglos_Matrices/Matrices.py
@@ -0,0 +1,18 @@
+matriz=[]#! Crea una lista/arreglo que sera nuestra matriz
+
+for i in range(3):#? Almacena en todos los elementos de la matriz 0
+    fila =[0 for i in range(3)]
+    matriz.append(fila)
+
+for f in matriz: #* Muestra los elementos de la matriz
+    print(f)
+
+for i in range (3): #? Cambia los elementos de la matriz por datos ingresados por el usuario
+    for j in range (3):
+        print("Posición:",i,",",j)
+        matriz[i][j]=int(input("Numero:"))
+
+for f in matriz: #* Muestra los elementos de la matriz
+    print(f)
+
+
